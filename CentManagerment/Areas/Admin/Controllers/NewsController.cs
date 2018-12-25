557cf402921c622ee1eefc472367216ae6ce4af7
Dung <mrvvip11@gmail.com>
2018-12-25 11:08:24 +0700

    Dũng fix db

--- CentManagerment/Areas/Admin/Controllers/NewsController.cs
+++ CentManagerment/Areas/Admin/Controllers/NewsController.cs
@@ -1,9 +1,6 @@
 ﻿using CentManagerment.BU.DataManager;
 using CentManagerment.BU.DTO;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace CentManagerment.Areas.Admin.Controllers
